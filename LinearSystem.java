57297c6613f233c43c3f6fb73ef19a5cee713ec4
Dorian <rakionplayerxd@gmail.com>
2020-11-12 12:18:07 -0400

    Realizadas algunas mejoras en condicionA1 , condicionAInfinito temporalmente desactivada

--- LinearSystem.java
+++ LinearSystem.java
@@ -9,7 +9,7 @@ public class LinearSystem
     public LinearSystem(double[][] matrix)
     {
         condicionA1(matrix);
-        condicionAInfinito(matrix);
+        //condicionAInfinito(matrix);
        
     }
 
@@ -18,13 +18,14 @@ public class LinearSystem
         double normaA1_t;
         //Condicion 1 es encontrar la normaUno de A y Multiplicarlo
         //por la normaUno de A^-1 (inversaMatrix)        
-        System.out.println("Norma A\u2081");
+        
         //Saco la normaUno de la matriz A
         normaA1 = norma.normaUno(matrix);
         //Saco la normaUno de la inversa de la matriz A (A^-1)
         normaA1_t = norma.normaUno(matrices.inversaMatrix(matrix));
         //Saco el producto de ambas para conseguir la Condicion_1
-        System.out.println(normaA1 * normaA1_t);
+        System.out.println("Condicion de A\u2081");
+        System.out.println("es: "+normaA1 * normaA1_t);
     }
     
     public void condicionA2(){
@@ -35,12 +36,13 @@ public class LinearSystem
         double normaAInfinito, normaAInfinito_t;
         //Condicion 1 es encontra la normaInfinito de A y Multiplicarlo
         //por la normaInfinito de A^-1 (inversaMatrix)
-        System.out.println("Norma A\u221E");
+        System.out.println("Condicion de A\u221E");
         //Saco la normaInfinito de la matriz A
         normaAInfinito = norma.normaInfinito(matrix);        
         //Saco la normaInfinito de la inversa de la matriz A (A^-1)
         normaAInfinito_t = norma.normaInfinito(matrices.inversaMatrix(matrix));
         //Saco el producto de ambas para conseguir la Condicion_Infinito
-        System.out.println(normaAInfinito * normaAInfinito_t);
+        System.out.println("Condicion de A\u221E");
+        System.out.println("es :"+normaAInfinito * normaAInfinito_t);
     }
 }
