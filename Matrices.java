9dc1a43e73945c622dd06454c70c188c14c5a69c
Dorian <rakionplayerxd@gmail.com>
2020-11-12 15:56:57 -0400

    Agregado metodo decimalToFraction para convertir los decimales a fracciones reducidas

--- Matrices.java
+++ Matrices.java
@@ -5,6 +5,7 @@ import org.apache.commons.math3.linear.DecompositionSolver;
 import org.apache.commons.math3.linear.LUDecomposition;
 import org.apache.commons.math3.linear.RealMatrix;
 import org.apache.commons.math3.linear.RealVector;
+import org.apache.commons.math3.fraction.Fraction;
 
 public class Matrices
 {
@@ -93,25 +94,38 @@ public class Matrices
     }
     
     public double[][] inversaMatrix(double[][] Matrix){
-        double [][] values = {{1, 1, 2}, {2, 4, -3}, {3, 6, -5}};
+        //Genero una matrix identidad del tamano necesario.
         double [][] identidad = generadorIdentidad(Matrix.length);       
+        //Declaro una matriz para almacenar la inversa.
         double [][] inversa;
         // Solving AB = I for given A
+        // Creo un objeto del tipo RealMatrix con los parametros y valores de la matriz 'Matrix'.
         RealMatrix A = new Array2DRowRealMatrix(Matrix);
-        System.out.println("Input A: " + A);
+        System.out.println("La matriz ingresada es : ");
+        //Creamos un String y almacenamos los datos del objeto RealMatrix A
+        //con el metodo toString() de la clase RealMatrix.
         String matrixA = A.toString();
-        String auxB;
-        //System.out.println(matrixA);
-        auxB = matrixA.replace("},", "} \n");
-        //System.out.println(auxB);
-        System.out.println(auxB.replace("Array2DRowRealMatrix",""));
-        //System.out.println(matrixA.replace("},", "}\n").replace("Array2DRowRealMatrix,"," "));
-        //System.out.println("Veamos "+ A);
-        DecompositionSolver solver = new LUDecomposition(A).getSolver();
-
+        //Creamos un segundo String auxiliar para almacenar los cambios realizados en el primer String matrixA.
+        //Realizamos reemplazos en el String auxB para darle el formato correcto.
+        String auxA = matrixA.replace("},", "} \n").replace("Array2DRowRealMatrix","");
+        //Imprimimos la matrix, previo formateo.
+        System.out.println(auxA);
+        //Magia. 
+        DecompositionSolver solver = new LUDecomposition(A).getSolver();        
+        //Le pasamos la matriz identidad para que se realice el procedimiento de inversion.
         RealMatrix I = new Array2DRowRealMatrix(identidad);
+        //Mas magia.
         RealMatrix B = solver.solve(I);
-        System.out.println("Inverse B: " + B);
+        //Creamos un String y almacenamos los datos del objeto RealMatrix A
+        //con el metodo toString() de la clase RealMatrix.
+        String identidadString = B.toString();
+        //Formateamos el String auxB para que muestre correctamente los datos.
+        String auxIdentidad = identidadString.replace("},", "} \n").replace("Array2DRowRealMatrix",""); 
+        //Imprimimos el resultado
+        System.out.println("La inversa de la matriz ingresada es : ");
+        System.out.println(auxIdentidad);
+        //Usamos el metodo gerData() que tiene la clase RealMatrix para poder obtener los valores de la matriz
+        //en un formato de matriz double[][].
         inversa = B.getData();
         return inversa;
     }
@@ -122,7 +136,13 @@ public class Matrices
         for(int i = 0; i < dimension;i++){
                 identidad[i][i] = 1;
         }
-        mostrarMatrix(identidad);
+        //No es necesario mostrar, pero comento la linea para fines de testeo
+        //mostrarMatrix(identidad);
         return identidad;
+    }
+    
+    public void decimalToFraction(double decimal){
+        Fraction f = new Fraction(decimal);
+        System.out.println(f);
     }
 }
